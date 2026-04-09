5bcbc5057739a1f897ef94263b20f0eb4a7b750b
RubenM <menorruben@gmail.com>
2026-04-09 17:09:01 +0200

    Añadi el operador typeof

--- JavaScript/Basic/02-datatypes.js
+++ JavaScript/Basic/02-datatypes.js
@@ -1,5 +1,5 @@
 //Strings
-let name="Ruben Menor";
+let myName="Ruben Menor";
 let alias='Iberobohemio';
 //Numeros
 let age= 25;    //enteros
@@ -15,4 +15,18 @@ let phoneNumber= null; //se le asigna un valor nulo, indicando que no tiene un v
 //Symbol
 let mySymbol= Symbol('mysymbol'); //crea un símbolo único con la descripcion que le demos
 //BigInt
-let bigNumber= 1234567890123456789012345678901234567890n; //n al final indica que es un BigInt
+let bigNumber= 1234567890123456789012345678901234567890n; //La n al final indica que es un BigInt
+
+
+//Vale ahora vamos a ver los tipos de datos con el operador typeof
+
+console.log(typeof myName); //string
+console.log(typeof age); //number
+console.log(typeof height); //number
+console.log(typeof isStudent); //boolean     
+console.log(typeof isTeacher); //boolean
+console.log(typeof address); //undefined
+console.log(typeof phoneNumber); //object (esto es un error histórico en JavaScript, null se considera un objeto)
+console.log(typeof mySymbol); //symbol
+console.log(typeof bigNumber); //bigint
+
