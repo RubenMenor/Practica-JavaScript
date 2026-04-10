f655de2f4ffe3d941e53f3cea853c25eb2c8e39a
RubenM <menorruben@gmail.com>
2026-04-10 22:47:47 +0200

    Visto Array y Set

---
+++ JavaScript/Basic/10-array.js
@@ -0,0 +1,71 @@
+//ARRAYS
+
+let myArray1_1=['Ruben',4,2, true]               //Es mejor trabajar con esta forma
+let myArray1_2=[1]
+let myArray2_1=new Array('Bohemio' , 3, 4, false)     //Esta forma reserva huecas, es mas tecnico a nivel de estructura de programacion, evitar usar a no ser que sea necesario
+let myArray2_2=new Array(1) 
+
+console.log(myArray1_1)
+console.log(myArray1_2)
+console.log(myArray2_1)
+console.log(myArray2_2)
+
+let myArray3=new Array(3)       //Da igual reservar 3 huecos, ya que puedes añadir los que quieras luego
+myArray3[0]='Menor'
+myArray3[1]=8
+myArray3[2]=true
+myArray3[3]=82993
+myArray3[4]='esto no da error'
+myArray3[5]='a no ser...'
+
+console.log(myArray3)
+
+
+//PUSH Y POP
+console.log('\nPUSH & POP')
+
+let myArray=[]
+
+myArray.push('Ruben')           //El comando push sirve para añador elementos al Array
+myArray.push('Menor')
+myArray.push('Mallorquin')
+myArray.push(29)
+myArray.push(true)
+console.log(myArray)
+
+console.log(myArray.pop())       //El comando pop elimina el ultimo elemento añadido y lo devuelve
+myArray.pop()                   
+console.log(myArray)
+
+console.log('SHIFT & UNSHIFT')
+console.log(myArray.shift())      //El comando shift hace lo mismo que el pop, pero con el primer elemento
+myArray.shift()
+console.log(myArray)
+
+myArray.unshift('Ruben', true, 30)
+console.log(myArray)
+
+
+//LENGTH
+//Esto sierve para ver el tamaño del Array
+console.log('LENGTH: ' + myArray.length)
+
+
+//CLEAR
+//No hay que poner esto[[ myArray.clear() ]] pues daria error, simplemente si queremos limpiar el Array, se vuelve a inicializar
+myArray=[]
+console.log('CLEAR: ' + myArray)
+myArray=['Ruben', 'Menor', 'Mallorquin', 29, true]
+
+//SLICE
+//Le indicamos "el pedazo(el slice)" que quiero, SIN TENER EN CUENTA LAS POSICIONES QUE PUSE
+
+let myNewArray = myArray.slice(1, 3)
+console.log('SLICE: ')
+console.log(myArray)
+console.log(myNewArray)
+
+//SPLICE
+
+myArray.splice(1, 3, 'Añado nuevo elemento')        //(Donde empiezo, cantidad a borrar, nuevo elemento añadido)
+console.log('SPLICE: ' + myArray)
---
+++ JavaScript/Basic/11-set.js
@@ -0,0 +1,50 @@
+//SETS
+
+//Declaracion
+let mySet=new Set()     
+
+//Inicializacion
+mySet=new Set(['Ruben', 'Menor', 'Mallorquin', 29, true])
+
+console.log(mySet)
+
+
+//--Metodos comunes--//
+
+//Add y delete
+
+mySet.add("menorruben")     //este add añade al final del Set
+console.log(mySet)
+
+mySet.delete("menorruben")     //elimina el elemento que elijas, pero se lo tienes que pasar tal cual, no vale con indices concretos
+console.log(mySet)
+ 
+console.log(mySet.delete('Ruben'))      //tambien retorna un boolean para confirmacion del comando, si lo haces directamente en el console.log
+
+
+//has
+//retorna un boolean para comprobar si lo buscado se encuentra en el Set
+console.log(mySet.has('Menor'))
+console.log(mySet.has('Ruben'))
+
+//size
+//manera de llamar al lenght
+console.log(mySet.size)
+
+
+//convertir Set en Array
+let myArray = Array.from(mySet)
+console.log(myArray)
+
+//concertir un Array en Set
+mySet = new Set(myArray)
+console.log(mySet)
+
+
+
+/* 
+
+Principal diferencia entre Array y Set:
+    EL ARRAY PERMITE DUPLICADOS, EL SET NO PERMITE DUPLICADOS(casesensitive)
+
+*/
